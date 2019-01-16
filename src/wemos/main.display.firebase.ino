1a43ebf3b19a46d131f546be9aa00783581edeff
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-01-16 22:59:12 +0200

    fix(wemos): do not inverse colors

--- src/wemos/main.display.firebase.ino
+++ src/wemos/main.display.firebase.ino
@@ -3,10 +3,57 @@
 
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include "config.h"
 
+// ---------------------------------------OLED CONFIG ------------------------------------- //
+// SCL GPIO5
+// SDA GPIO4
+#define OLED_RESET 0  // GPIO0
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+#if (SSD1306_LCDHEIGHT != 48)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup_display() {
+
+   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+  // init done
+}
+
+void display_text(String text) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  // display.setTextColor(BLACK, WHITE); 
+  display.println(text);
+  // display.setTextSize(2);
+  // display.setTextColor(WHITE);
+  display.display();
+
+}
+// --------------------------------------- Main ------------------------------------- //
 void setup() {
+  
   Serial.begin(115200);
+  // init display 
+  setup_display();
 
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -27,7 +74,8 @@ void setup() {
 void loop() {
   // get value 
   Serial.print("reading from firebase: ");
-  Serial.println(Firebase.getString("message"));
-
+  String fbText = String(Firebase.getString("message"));
+  Serial.println(fbText);
+  display_text(fbText);
   delay(2000);
 }
